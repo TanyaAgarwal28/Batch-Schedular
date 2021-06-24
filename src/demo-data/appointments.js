9a9954ec417a5e212f7d3013790461d214951100
Tanya Agarwal <tanya.agrawal1_cs19@gla.ac.in>
2021-06-24 17:06:08 +0530

    initial commit

--- src/demo-data/appointments.js
+++ src/demo-data/appointments.js
@@ -3,48 +3,48 @@ export const appointments = [
     title: "Operating System",
     startDate: new Date(2021, 5, 21, 10, 30),
     endDate: new Date(2021, 5, 21, 11, 30),
-    teacherName: "Dhirendre Kumar Shukla"
+    teacher: [1]
   },
   {
     title: "Data Structure and Algorithm",
     startDate: new Date(2021, 5, 21, 12, 0),
     endDate: new Date(2021, 5, 21, 13, 0),
-    teacherName: "Divyansh"
+    teacher: [2]
   },
   {
     title: "Microprocessor",
     startDate: new Date(2021, 5, 21, 14, 30),
     endDate: new Date(2021, 5, 21, 15, 30),
-    teacherName: "Deepak Mittal"
+    teacher: [6]
   },
   {
     title: "Mathematics",
     startDate: new Date(2021, 5, 21, 13, 0),
     endDate: new Date(2021, 5, 21, 14, 0),
-    teacherName: "Pooja"
+    teacher: [3]
   },
   {
     title: "English",
     startDate: new Date(2021, 5, 24, 10, 0),
     endDate: new Date(2021, 5, 24, 11, 35),
-    teacherName: "Divya"
+    teacher: [7]
   },
   {
     title: "DCS",
     startDate: new Date(2021, 5, 24, 12, 0),
     endDate: new Date(2021, 5, 24, 13, 35),
-    teacherName: "Anupam"
+    teacher: [8]
   },
   {
     title: "Computer Organization",
     startDate: new Date(2021, 5, 25, 10, 0),
     endDate: new Date(2021, 5, 25, 11, 35),
-    teacherName: "Juginder"
+    teacher: [9]
   },
   {
     title: "Java",
     startDate: new Date(2021, 5, 24, 14, 0),
     endDate: new Date(2021, 5, 24, 15, 35),
-    teacherName: "Divyansh"
+    teacher: [5]
   }
 ];
